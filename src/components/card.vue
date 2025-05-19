f5a55ee5619f1538d238272082af7306c1a9ea20
Daniel… <danieloluwafolajimi@gmail.com>
2025-05-19 12:29:03 +0100

    Remove package-lock.json, update package.json to include sass-embedded, and modify yarn.lock for dependency resolutions. Refactor App.vue and HelloWorld.vue components, enhancing styles and structure. Update style.css for background color and overlay effects.

---
+++ src/components/card.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="card">
+    <img :src="props.card.image" alt="card" />
+    <div>
+      <div>
+        <p>{{ props.card.location }}</p>
+        <p>{{ props.card.sub_location }}</p>
+      </div>
+      <div>
+        <button>
+          <img src="@/assets/images/icons/play.svg" alt="play" />
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+interface Card {
+  id: number
+  location: string
+  sub_location: string
+  image: string
+  mode: string
+}
+
+const props = defineProps<{
+  card: Card
+}>()
+</script>
